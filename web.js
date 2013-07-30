5eb3989f71d5ff9b67fa727a74764e4a01771670
Atabek Akbalaev <atabek.akbalaev@gmail.com>
2013-07-29 19:10:50 -0500

    Port 8080 is made default

--- web.js
+++ web.js
@@ -1,13 +1,14 @@
 var express = require('express');
-var app = express.createServer(express.logger());
 var fs = require('fs');
-var data = fs.readFileSync("./index.html", 'utf8');
+var htmlfile = "index.html";
 
-app.get('/', function(request, response) {
-  response.send(data);
+var app = express.createServer(express.logger());
+app.get('/', function(request, response){
+	var html = fs.readFileSync(htmlfile).toString();
+	response.send(html);
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+var port = process.env.PORT || 8080;
+app.listen(port, function(){
+	console.log("Listening on " + port);
 });
